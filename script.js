4d874789e2c1d23214752243c4dc2f257e363913
NicoleSoares244 <00001115457639sp@al.educacao.sp.gov.br>
2025-08-19 11:15:10 -0300

    v1

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+
+    let tamanhoAtualFonte= 1;
+
+        aumentaFonteBotao.addEventListener('clic', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize =`${tamanhoAtualFonte}rem`
+   })
+        aumentaFonteBotao.addEventListener('clic', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize =`${tamanhoAtualFonte}rem`
+
+
+})
